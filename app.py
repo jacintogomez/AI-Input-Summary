c21d30c50f607e56f2dfb08fcf47a9febe16638d
jacintogomez <63368475+jacintogomez@users.noreply.github.com>
2024-03-24 02:32:57 -0400

    LangChain AI functionality added in

--- app.py
+++ app.py
@@ -1,5 +1,19 @@
 from flask import Flask,render_template,request
+from dotenv import load_dotenv
+from langchain_openai import ChatOpenAI
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+from langchain_openai import OpenAIEmbeddings
+from langchain_community.vectorstores import FAISS
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.chains.combine_documents import create_stuff_documents_chain
+from langchain.chains import create_retrieval_chain
+from langchain_community.document_loaders import TextLoader
+
+import textwrap
 import os
+load_dotenv()
+openai_api_key = os.getenv("OPENAI_API_KEY")
 
 app = Flask(__name__)
 
@@ -23,11 +37,10 @@ def process():
             print('pdf file')
             summary=langpdf(filename)
         elif filename.endswith('.txt'):
-            text=''
-            with open(os.path.join('uploads',filename),'r') as file:
-                text=file.read()
-            print('txt file: ',text)
-            summary=langtxt(text)
+            # with open(os.path.join('uploads',filename),'r') as file:
+            #     text=file.read()
+            # print('txt file: ',text)
+            summary=langtxt(filename)
         else:
             print('Not supported')
             summary='File type not supported'
@@ -37,8 +50,38 @@ def process():
 def langpdf(file):
     return 'answer'
 
-def langtxt(text):
-    return text.upper()
+def langtxt(file):
+    llm=ChatOpenAI()
+    prompt=ChatPromptTemplate.from_template("""Answer the following request based only on the provided context:
+    
+    <context>
+    {context}
+    </context>
+    
+    Question: {input}""")
+    documentchain=create_stuff_documents_chain(llm,prompt)
+    outputparser=StrOutputParser()
+    chain=prompt|llm|outputparser
+    loader=TextLoader(os.path.join('uploads',file))
+    docs=loader.load()
+
+    embeddings=OpenAIEmbeddings()
+    tsplitter=RecursiveCharacterTextSplitter()
+    documents=tsplitter.split_documents(docs)
+    vec=FAISS.from_documents(documents,embeddings)
+
+    retriever=vec.as_retriever()
+    retrievalchain=create_retrieval_chain(retriever,documentchain)
+    question='Please summarize the given file in 5 sentences or less'
+    response=retrievalchain.invoke({'input':question})
+    print(question)
+    cleanans=response['answer']
+    print(consoleformat(cleanans))
+    return cleanans
+
+def consoleformat(input,width=100):
+    formatted=textwrap.fill(input,width)
+    return formatted
 
 if __name__ == '__main__':
     app.run(debug=True)
